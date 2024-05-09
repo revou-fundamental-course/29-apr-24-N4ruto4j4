177016ad644156071831db45db043bd3468755ef
Abdur Razak Asrofi <abdurrazakasrofi99@gmail.com>
2024-05-09 22:45:27 +0700

    halaman home done

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// slider home
+
 var slideIndex = 1;
 showDivs(slideIndex);
 
@@ -19,3 +21,61 @@ function showDivs(n){
 setInterval(() => {
     plusDivs(1)
 }, 5000)
+
+// slider message
+var slideIndexMessage = 1;
+
+showDivsMessage(slideIndexMessage);
+
+function plusDivsMessage(n){
+    showDivsMessage((slideIndexMessage += 1))
+}
+
+function showDivsMessage(n){
+    var i;
+    var imgListMessage=document.getElementsByClassName("img-slide-message");
+    if( n > imgListMessage.length) slideIndexMessage = 1;
+    else if ( n < 1 ) slideIndexMessage = imgListMessage.length;
+    for( i = 0; i < imgListMessage.length; i++ ){
+        imgListMessage[i].style.display = "none";
+    }
+    imgListMessage[slideIndexMessage-1].style.display = "block";
+}
+
+setInterval(() => {
+    plusDivsMessage(1)
+}, 5000)
+
+// alert unable
+
+function unable(){
+    window.alert('Maaf! fitur ini belum tersedia');
+}
+
+// form validation
+
+function check_form(){
+    var username = document.getElementById("username").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message-text").value;
+
+    if(username == "" || email == "" || message == ""){
+        document.getElementById("error").style.display="inline-block";
+        document.getElementById("username").style.border="1px solid #ff6309";
+        document.getElementById("email").style.border="1px solid #ff6309";
+        document.getElementById("message-text").style.border="1px solid #ff6309";
+    }else{
+        alert("Successed to login");
+        document.getElementById("error").style.display="none";
+        document.getElementById("username").style.border="0px";
+        document.getElementById("email").style.border="0px";
+        document.getElementById("message-text").style.border="0px";
+        
+    }
+
+}
+
+
+
+
+
